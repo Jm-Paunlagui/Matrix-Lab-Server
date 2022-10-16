647a6e1b71971d193249fe6c1555faaf8d9c9de8
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2022-10-16 22:54:51 +0800

    Set to true

--- models/user_model.py
+++ models/user_model.py
@@ -22,7 +22,7 @@ class User(db.Model):
     __tablename__ = 'users'
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(255), unique=True, nullable=False)
-    recovery_email = db.Column(db.String(255), unique=True, nullable=False)
+    recovery_email = db.Column(db.String(255), unique=True, nullable=True)
     first_name = db.Column(db.String(255), nullable=False)
     last_name = db.Column(db.String(255), nullable=False)
     username = db.Column(db.String(255), unique=True, nullable=False)
