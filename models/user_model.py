b2ad5540a9215faa6cd8cad9cf44754b0ce499f5
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2022-10-26 22:58:02 +0800

    patch to model

--- models/user_model.py
+++ models/user_model.py
@@ -8,6 +8,8 @@ class User(db.Model):
     User model class attributes
     user_id: User id number (primary key) (auto increment) bigint
     email: User email address (unique) varchar(255)
+    secondary_email: User secondary email address (unique) varchar(255)
+    recovery_email: User recovery email address (unique) varchar(255)
     first_name: User first name varchar(255)
     last_name: User last name varchar(255)
     username: User username (unique) varchar(255)
@@ -22,7 +24,8 @@ class User(db.Model):
 
     __tablename__ = 'users'
     user_id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    email: str = db.Column(db.String(255), unique=True, nullable=False)
+    email: str = db.Column(db.String(255), unique=True, nullable=True)
+    secondary_email: str = db.Column(db.String(255), unique=True, nullable=True)
     recovery_email: str = db.Column(db.String(255), unique=True, nullable=True)
     first_name: str = db.Column(db.String(255), nullable=False)
     last_name: str = db.Column(db.String(255), nullable=False)
@@ -38,6 +41,6 @@ class User(db.Model):
 
     def __repr__(self):
         """User model class representation."""
-        return f"User('{self.user_id}', '{self.email}', '{self.recovery_email}', '{self.first_name}', " \
-               f"'{self.last_name}', '{self.username}', '{self.password}', '{self.role}', '{self.created_at}', " \
-               f"'{self.updated_at}', '{self.flag_deleted}', '{self.password_reset_token}')"
+        return f"User('{self.user_id}', '{self.email}', '{self.secondary_email}', '{self.recovery_email}', " \
+               f"'{self.first_name}', '{self.last_name}', '{self.username}', '{self.password}', '{self.role}', " \
+               f"'{self.created_at}', '{self.updated_at}', '{self.flag_deleted}', '{self.password_reset_token}')"
