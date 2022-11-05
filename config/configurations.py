8e3ca8c41caac5f8de3553c58ad6a2a0a1126594
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2022-11-05 09:34:29 +0800

    double quotes

--- config/configurations.py
+++ config/configurations.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 app.app_context().push()
 
 # @desc: Email configuration for the Flask app
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config["MAIL_SERVER"] = 'smtp.gmail.com'
 app.config["MAIL_USE_SSL"] = True
 app.config["MAIL_USE_TLS"] = False
 app.config["MAIL_PORT"] = 465
