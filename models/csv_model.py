23a5765cf44e2a11469ac1dd91ee255ee20af85d
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2022-11-11 00:07:04 +0800

    Added Semester

--- models/csv_model.py
+++ models/csv_model.py
@@ -10,6 +10,7 @@ class CsvModel(db.Model):
     csv_question: Csv question varchar(255)
     csv_file_path: Csv file path text
     school_year: School year varchar(255)
+    school_semester: School semester varchar(255)
     date_uploaded: Csv date uploaded timestamp
     date_processed: Csv date processed timestamp
     """
@@ -20,6 +21,7 @@ class CsvModel(db.Model):
     csv_question: str = db.Column(db.String(255), nullable=False)
     csv_file_path: str = db.Column(db.Text, nullable=False)
     school_year: str = db.Column(db.String(255), nullable=False)
+    school_semester: str = db.Column(db.String(255), nullable=False)
     date_uploaded: str = db.Column(db.DateTime, nullable=False,
                                    default=Timezone("Asia/Manila").get_timezone_current_time())
     date_processed: str = db.Column(db.DateTime, nullable=False,
@@ -29,4 +31,5 @@ class CsvModel(db.Model):
         """Csv model class representation."""
         return f"CsvModel(csv_id={self.csv_id}, csv_name={self.csv_name}, csv_question={self.csv_question}, " \
                f"csv_file_path={self.csv_file_path}, school_year={self.school_year}, " \
-               f"date_uploaded={self.date_uploaded}, date_processed={self.date_processed})"
+               f"school_semester={self.school_semester}, date_uploaded={self.date_uploaded}, " \
+               f"date_processed={self.date_processed})"
