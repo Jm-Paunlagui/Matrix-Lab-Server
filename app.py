992167329715b4eee0b1af192f157fa18cdf68f7
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2023-02-05 21:44:10 +0800

    docker and procfile

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ app.register_blueprint(predictalyze)
 app.register_blueprint(dashboard)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, load_dotenv=True)
